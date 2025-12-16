b3ff41650665f58afa4af280eb02510d7d03a254
orion-3464 <nikolaoskor8@gmail.com>
2025-12-16 12:15:09 +0200

    one bug on starting point

--- part1/ex1.py
+++ part1/ex1.py
@@ -9,6 +9,9 @@ def grad_f(x, y):
     grady = 2*(y-1) - 4*(x-y)**3
     return np.array([gradx, grady])
 
+def grad2_f(x, y):
+    return 4 + 24*(x-y)**2 + 1e-6 # avoid making zero denominator
+    
 def himmelblau(x, y):
     return np.square(np.square(x)+y-11) + np.square(x+np.square(y))
 
@@ -16,6 +19,9 @@ def himmelblau_grad(x, y):
     gradx = 4*(x**2+y-11)*x + 2*(x+y**2)
     grady = 2*(x**2+y-11) + 4*(x+y**2)*y
     return np.array([gradx, grady])
+
+def himmelblau_grad2(x, y):
+    return 4*(-17 + x + 3*x**2 + y + 3*y**2) + 1e-6
 
 def rosenbrock(x, y):
     a = 1
@@ -29,6 +35,9 @@ def rosenbrock_grad(x, y):
     grady = 2*b*(y-x**2)
     return np.array([gradx, grady])
 
+def rosenbrock_grad2(x, y):
+    return 202 +  1200*x**2 - 400*y + 1e-6
+
 def GD(grad, x=np.zeros(2, dtype=np.float32), lr=1e-3, max_iter=10000):
     path = []
     x_prev = np.ones(2)*1000
@@ -38,7 +47,7 @@ def GD(grad, x=np.zeros(2, dtype=np.float32), lr=1e-3, max_iter=10000):
         print(f"Iteration {i+1}: x = {x[0]:.4f}, y = {x[1]:.4f}")
         
         if np.linalg.norm(x-x_prev, ord=2) < 1e-4:
-            print("Converged")
+            print(f"Converged (took {i+1} iterations)")
             break
         
         x_prev = x.copy()
@@ -46,10 +55,24 @@ def GD(grad, x=np.zeros(2, dtype=np.float32), lr=1e-3, max_iter=10000):
         
     return x, path
 
-def Newton():
-    pass
+def Newton(grad, grad2, x=np.zeros(2, dtype=np.float32), max_iter=100):
+    path = []
+    x_prev = np.ones(2)*1000
 
-def VisualiseGD(func, X, Y, path):
+    for i in range(max_iter):
+        x = x - (1 / grad2(x[0], x[1])) * grad(x[0], x[1])
+        print(f"Iteration {i+1}: x = {x[0]:.4f}, y = {x[1]:.4f}")
+        
+        if np.linalg.norm(x-x_prev, ord=2) < 1e-4:
+            print(f"Converged (took {i+1} iterations)")
+            break
+        
+        x_prev = x.copy()
+        path.append(x)
+        
+    return x, path
+
+def Visualise(func, X, Y, path, algo):
     xg, yg = np.meshgrid(X, Y)
     Z = func(xg, yg)
 
@@ -59,18 +82,31 @@ def VisualiseGD(func, X, Y, path):
     cnt = plt.contour(xg, yg, Z, levels=150)
     plt.colorbar(cnt)
 
-    if len(path) > 0:
-        plt.plot(path[:,0], path[:,1], 'r.-', label='GD Path')
-        plt.scatter(path[0,0], path[0,1], c='green', label='Start', s=80)
-        plt.scatter(path[-1,0], path[-1,1], c='red', label='End', s=80)
+    if algo=="gd":
+        if len(path) > 0:
+            plt.plot(path[:,0], path[:,1], 'r.-', label='GD Path')
+            plt.scatter(path[0,0], path[0,1], c='green', label='Start', s=80)
+            plt.scatter(path[-1,0], path[-1,1], c='red', label='End', s=80)
 
-    plt.title("Gradient Descent Trajectory")
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+        plt.title("Gradient Descent Trajectory")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+    else:
+        if len(path) > 0:
+            plt.plot(path[:,0], path[:,1], 'r.-', label='Newton Path')
+            plt.scatter(path[0,0], path[0,1], c='green', label='Start', s=80)
+            plt.scatter(path[-1,0], path[-1,1], c='red', label='End', s=80)
 
+        plt.title("Newton's Method Trajectory")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+        
 
 if __name__ == "__main__":
     # Function F
@@ -83,8 +119,11 @@ if __name__ == "__main__":
     start = np.array([x0, y0])
 
     x_final, path = GD(grad_f, x=start)
-    VisualiseGD(f, X, Y, path)
+    Visualise(f, X, Y, path, algo="gd")
 
+    x_final, path = Newton(grad_f, grad2_f, x=start)
+    Visualise(f, X, Y, path, algo="nt")
+    
     # Funciton Himmelblau
     X = np.linspace(-5, 5, 1000, dtype=np.float32)
     Y = np.linspace(-5, 5, 1000, dtype=np.float32)
@@ -95,7 +134,10 @@ if __name__ == "__main__":
     start = np.array([x0, y0])
 
     x_final, path = GD(himmelblau_grad, x=start)
-    VisualiseGD(himmelblau, X, Y, path)
+    Visualise(himmelblau, X, Y, path, algo="gd")
+
+    x_final, path = Newton(himmelblau_grad, himmelblau_grad2, x=start)
+    Visualise(himmelblau, X, Y, path, algo="nt")
 
     # Function Rosenbrock
     X = np.linspace(-1, 1, 1000, dtype=np.float32)
@@ -107,4 +149,7 @@ if __name__ == "__main__":
     start = np.array([x0, y0])
 
     x_final_r, path_r = GD(rosenbrock_grad, x=start)
-    VisualiseGD(rosenbrock, X, Y, path_r)
+    Visualise(rosenbrock, X, Y, path_r, algo="gd")
+
+    x_final_r, path_r = Newton(rosenbrock_grad, rosenbrock_grad2, x=start, max_iter=3000)
+    Visualise(rosenbrock, X, Y, path_r, algo="nt")

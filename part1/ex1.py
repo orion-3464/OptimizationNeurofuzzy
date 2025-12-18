9e8a3d5b82ec0e9bfe2f88b483a1a6c33d92ac57
orion-3464 <nikolaoskor8@gmail.com>
2025-12-18 21:28:42 +0200

    finished thema 1

--- part1/ex1.py
+++ part1/ex1.py
@@ -18,11 +18,11 @@ def f_hessian(x, y):
                      [fxy, fyy]])
     
 def himmelblau(x, y):
-    return np.square(np.square(x)+y-11) + np.square(x+np.square(y))
+    return np.square(x**2+y-11) + np.square(x+y**2-7)
 
 def himmelblau_grad(x, y):
-    gradx = 4*(x**2+y-11)*x + 2*(x+y**2)
-    grady = 2*(x**2+y-11) + 4*(x+y**2)*y
+    gradx = 4*x*(x**2+y-11) + 2*(x+y**2-7) 
+    grady = 2*(x**2+y-11) + 4*y*(x+y**2-7)
     return np.array([gradx, grady])
 
 def himmelblau_hessian(x, y):
@@ -51,7 +51,7 @@ def rosenbrock_hessian(x, y):
     return np.array([[fxx, fxy], 
                      [fxy, fyy]])
 
-def GD(grad, x=np.zeros(2, dtype=np.float32), lr=1e-3, max_iter=10000):
+def GD(grad, x=np.zeros(2, dtype=np.float32), lr=1e-2, max_iter=10000):
     x = x.copy()
     path = [x]
     x_prev = np.ones(2)*1000
@@ -60,7 +60,7 @@ def GD(grad, x=np.zeros(2, dtype=np.float32), lr=1e-3, max_iter=10000):
         x = x - lr*grad(x[0], x[1])
         #print(f"Iteration {i+1}: x = {x[0]:.4f}, y = {x[1]:.4f}")
         
-        if np.linalg.norm(x-x_prev, ord=2) < 1e-4:
+        if np.linalg.norm(x-x_prev, ord=2) < 1e-5:
             print(f"Converged (took {i+1} iterations)")
             break
         
@@ -96,7 +96,7 @@ def Newton(grad, hessian, x=np.zeros(2, dtype=np.float32), max_iter=100):
         
     return x, path
 
-def Visualise(func, X, Y, path, algo):
+def Visualise(func, X, Y, path, algo, limits, lr=None):
     xg, yg = np.meshgrid(X, Y)
     Z = func(xg, yg)
 
@@ -108,19 +108,21 @@ def Visualise(func, X, Y, path, algo):
 
     if algo=="gd":
         if len(path) > 0:
-            plt.plot(path[:,0], path[:,1], 'r.-', label='GD Path')
+            plt.plot(path[:,0], path[:,1], 'b.-', label='GD Path')
             plt.scatter(path[0,0], path[0,1], c='green', label='Start', s=80)
             plt.scatter(path[-1,0], path[-1,1], c='red', label='End', s=80)
 
-        plt.title("Gradient Descent Trajectory")
+        plt.title(f"Gradient Descent Trajectory (lr = {lr})")
         plt.xlabel("x")
         plt.ylabel("y")
         plt.legend()
         plt.grid(True)
+        plt.xlim(limits[0], limits[1])
+        plt.ylim(limits[2], limits[3])
         plt.show()
     else:
         if len(path) > 0:
-            plt.plot(path[:,0], path[:,1], 'r.-', label='Newton Path')
+            plt.plot(path[:,0], path[:,1], 'b.-', label='Newton Path')
             plt.scatter(path[0,0], path[0,1], c='green', label='Start', s=80)
             plt.scatter(path[-1,0], path[-1,1], c='red', label='End', s=80)
 
@@ -129,6 +131,8 @@ def Visualise(func, X, Y, path, algo):
         plt.ylabel("y")
         plt.legend()
         plt.grid(True)
+        plt.xlim(limits[0], limits[1])
+        plt.ylim(limits[2], limits[3])
         plt.show()
         
 
@@ -143,37 +147,47 @@ if __name__ == "__main__":
     start = np.array([x0, y0])
 
     x_final, path = GD(grad_f, x=start)
-    Visualise(f, X, Y, path, algo="gd")
+    Visualise(f, X, Y, path, algo="gd", limits=[-2, 2, -2, 2])
+    print(f"Found function's minimum at x = {x_final[0]}, y = {x_final[1]} using gradient descend method.")
+    print(f"Its value is {f(x_final[0], x_final[1])}")
 
     x_final, path = Newton(grad_f, f_hessian, x=start)
-    Visualise(f, X, Y, path, algo="nt")
-    
+    Visualise(f, X, Y, path, algo="nt", limits=[-2, 2, -2, 2])
+    print(f"Found function's minimum at x = {x_final[0]}, y = {x_final[1]} using Newton's method.")
+    print(f"Its value is {f(x_final[0], x_final[1])}")
+
     # Funciton Himmelblau
-    X = np.linspace(-7, 7, 1000, dtype=np.float32)
-    Y = np.linspace(-7, 7, 1000, dtype=np.float32)
-
-    np.random.seed(17)
-    x0 = np.random.choice(X)
-    y0 = np.random.choice(Y)
-    start = np.array([x0, y0])
-
-    x_final, path = GD(himmelblau_grad, x=start)
-    Visualise(himmelblau, X, Y, path, algo="gd")
-
-    x_final, path = Newton(himmelblau_grad, himmelblau_hessian, x=start)
-    Visualise(himmelblau, X, Y, path, algo="nt")
-
-    # Function Rosenbrock
-    X = np.linspace(-3, 3, 1000, dtype=np.float32)
-    Y = np.linspace(-3, 3, 1000, dtype=np.float32)
+    X = np.linspace(-5, 5, 1000, dtype=np.float32)
+    Y = np.linspace(-5, 5, 1000, dtype=np.float32)
 
     np.random.seed(0)
     x0 = np.random.choice(X)
     y0 = np.random.choice(Y)
     start = np.array([x0, y0])
 
-    x_final_r, path_r = GD(rosenbrock_grad, x=start)
-    Visualise(rosenbrock, X, Y, path_r, algo="gd")
+    x_final_h, path = GD(himmelblau_grad, x=start)
+    Visualise(himmelblau, X, Y, path, algo="gd", limits=[-5, 5, -5, 5], lr=0.01)
+    print(f"Found Himmelblau's function minimum at x = {x_final_h[0]}, y = {x_final_h[1]} using gradient descend method.")
+    print(f"Its value is {himmelblau(x_final_h[0], x_final_h[1])}")
 
-    x_final_r, path_r = Newton(rosenbrock_grad, rosenbrock_hessian, x=start, max_iter=3000)
-    Visualise(rosenbrock, X, Y, path_r, algo="nt")
+    x_final_h, path = Newton(himmelblau_grad, himmelblau_hessian, x=start)
+    Visualise(himmelblau, X, Y, path, algo="nt", limits=[-5, 5, -5, 5])
+    print(f"Found Himmelblau's function minimum at x = {x_final_h[0]}, y = {x_final_h[1]} using Newton's method.")
+    print(f"Its value is {himmelblau(x_final_h[0], x_final_h[1])}")
+
+    # Function Rosenbrock
+    X = np.linspace(-1, 2, 1000, dtype=np.float32)
+    Y = np.linspace(-0.2, 2, 1000, dtype=np.float32)
+
+    np.random.seed(10)
+    x0 = np.random.choice(X)
+    y0 = np.random.choice(Y)
+    start = np.array([x0, y0])
+
+    x_final_r, path_r = GD(rosenbrock_grad, x=start, lr=1e-4, max_iter=500000)
+    Visualise(rosenbrock, X, Y, path_r, algo="gd", limits=[-1, 2, -0.2, 2], lr=5e-4)
+    print(f"Found Rosenbrock's function minimum at x = {x_final_r[0]}, y = {x_final_r[1]} using gradient descend method.")
+
+    x_final_r, path_r = Newton(rosenbrock_grad, rosenbrock_hessian, x=start)
+    Visualise(rosenbrock, X, Y, path_r, algo="nt", limits=[-1, 2, -0.2, 2])
+    print(f"Found Rosenbrock's function minimum at x = {x_final_r[0]}, y = {x_final_r[1]} using Newton's method.")
